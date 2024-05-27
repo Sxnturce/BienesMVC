02d7cdec9664b99fd3c5a976cf29bdbef95a2057
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-05-26 21:57:25 -0500

    Se encripto y agrego alertas para los inputs

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -25,22 +25,49 @@ const registrar = async (req, res) => {
   await check('nombre').notEmpty().withMessage("El nombre es obligatorio").run(req)
   await check('email').isEmail().withMessage("Ingrese un email valido").run(req)
   await check('pass').isLength({ min: 6 }).withMessage("El password debe ser de al menos 6 caracteres").run(req)
-  await check('repet_pass').equals('pass').withMessage("Los passwords no son iguales").run(req)
+  await check('repet_pass').custom((value, { req }) => value === req.body.pass).withMessage('Las contraseñas no son iguales').run(req);
 
   //Verifica si el resultado esta vacio
   let resultado = validationResult(req)
+
+  /*   res.json(resultado.array()) */
   if (!resultado.isEmpty()) {
     return res.render("auth/registro", {
       pagina: "Crear Cuenta",
       errores: resultado.array(),
       usuario: {
         name: req.body.nombre,
-        email: req.body.email,
+        email: req.body.email
       }
     })
   }
-  const usuario = await Usuario.create(req.body)
-  res.json(usuario)
+
+  //Desestructuramos 
+  const { nombre, email, pass } = req.body
+
+  //Verificar que no este duplicado
+  const existUser = await Usuario.findOne({ where: { email } })
+
+  if (existUser) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      errores: [{
+        msg: "El email ya esta registrado",
+        registered: true
+      }],
+      usuario: {
+        name: req.body.nombre,
+        email: req.body.email
+      }
+    })
+  }
+
+  await Usuario.create({
+    nombre,
+    email,
+    pass,
+    token: 123
+  })
 }
 
 export {
